aa6869e3bdf2b504caef98d8d21480436d2ba9c9
Fogrex <fogrexon@gmail.com>
2023-12-30 17:07:43 +0900

    fix: move package directory

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Character: MonoBehaviour{
+    public void OnTagAttach()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnTagDetach()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Awake()
+    {
+        var baseTag = ReactiveTagRoot;
+
+        
+    }
+}
